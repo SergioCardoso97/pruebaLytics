46fabcf2ad98a5c2449089279b8860aa1e0a8443
Sergio Cardoso <sergio.cardhdez97@gmail.com>
2021-12-30 15:42:44 -0600

    Prueba de Conocimientos segunda version

--- pruebaDeConocimientos.py
+++ pruebaDeConocimientos.py
@@ -1,4 +1,3 @@
-from numpy import fabs
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -8,6 +7,8 @@ import matplotlib.pyplot as plt
 def run():
     data = pd.read_csv("covid-19-data/us.csv")
     casosTop10 = data.sort_values(["cases"],ascending=False)
+    print(pd.unique(data["cases"]))
+    print("*************************************************")
     print(casosTop10)
     grafica = casosTop10.head(10).plot("date","cases")
     plt.savefig('Top10Casos.png')
