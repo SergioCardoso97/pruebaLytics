9669167744e91df5198b93cc56bf53c809e2e814
Sergio Cardoso <sergio.cardhdez97@gmail.com>
2021-12-30 15:31:30 -0600

    Prueba de Conocimientos

---
+++ pruebaDeConocimientos.py
@@ -0,0 +1,16 @@
+from numpy import fabs
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+
+def run():
+    data = pd.read_csv("covid-19-data/us.csv")
+    casosTop10 = data.sort_values(["cases"],ascending=False)
+    print(casosTop10)
+    grafica = casosTop10.head(10).plot("date","cases")
+    plt.savefig('Top10Casos.png')
+
+if __name__ == '__main__':
+    run() 
